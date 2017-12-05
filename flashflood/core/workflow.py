96c2377b7fe4cddf163f010bbc8bb6f1b7bf8076
Seiji Matsuoka <mojaie@aol.com>
2017-12-05 17:35:26 +0900

    core.subworkflow

--- flashflood/core/workflow.py
+++ flashflood/core/workflow.py
@@ -4,11 +4,8 @@
 # http://opensource.org/licenses/MIT
 #
 
-import time
-
 from tornado import gen
 
-from flashflood import static
 from flashflood.util import graph
 from flashflood.core.node import Asynchronizer
 from flashflood.core.task import Task
@@ -26,10 +23,21 @@ class Workflow(Task):
         # TODO: verbose output
         self.verbose = verbose
 
-    @gen.coroutine
-    def submit(self):
-        self.on_submitted()
-        yield self.run()
+    def on_submitted(self):
+        # TODO: catch exceptions on submit (ex. file path error, format error)
+        if not self.nodes:
+            raise ValueError("No nodes are registered")
+        elif len(self.nodes) == 1:
+            self.order = [self.nodes[0].node_num]
+            self.nodes[0].on_submitted()
+        else:
+            self.order = graph.topological_sort(self.succs, self.preds)
+            for up in self.order:
+                self.nodes[up].on_submitted()
+                for down, dport in self.succs[up].items():
+                    uport = self.preds[down][up]
+                    self.nodes[down].add_in_edge(
+                        self.nodes[up].out_edge(uport), dport)
 
     @gen.coroutine
     def run(self):
@@ -41,6 +49,22 @@ class Workflow(Task):
                 return
             yield gen.sleep(self.interval)
         self.on_finish()
+
+    @gen.coroutine
+    def interrupt(self):
+        self.status = "interrupted"
+        # TODO: Only an Asynchronizer might be running which will be stopped
+        # TODO: sqlitewriter should allow interruption
+        fs = [
+            n.interrupt() for n in self.nodes if isinstance(n, Asynchronizer)]
+        while not all(f.done() for f in fs):
+            yield gen.sleep(self.interval)
+        self.on_aborted()
+
+    @gen.coroutine
+    def submit(self):
+        self.on_submitted()
+        yield self.run()
 
     def connect(self, up, down, up_port=0, down_port=0):
         """Adds a workflow connection
@@ -62,23 +86,50 @@ class Workflow(Task):
         self.preds[down.node_num][up.node_num] = up_port
         self.succs[up.node_num][down.node_num] = down_port
 
-    @gen.coroutine
-    def interrupt(self):
-        self.status = "interrupted"
-        # TODO: Only an Asynchronizer might be running which will be stopped
-        # TODO: sqlitewriter should allow interruption
-        fs = [
-            n.interrupt() for n in self.nodes if isinstance(n, Asynchronizer)]
-        while not all(f.done() for f in fs):
-            yield gen.sleep(self.interval)
-        self.on_aborted()
+
+class SubWorkflow(Workflow):
+    def __init__(self, params=None, verbose=False):
+        super().__init__(verbose=verbose)
+        self.node_num = None
+        self._in_edge = None
+        self._out_edge = None
+        self._entrance = None
+        self._entrance_port = None
+        self._exit = None
+        self._exit_port = None
+        if params is not None:
+            self.params.update(params)
 
     def on_submitted(self):
-        # TODO: catch exceptions on submit (ex. file path error, format error)
-        self.order = graph.topological_sort(self.succs, self.preds)
-        for up in self.order:
-            for down, dport in self.succs[up].items():
-                uport = self.preds[down][up]
-                self.nodes[down].add_in_edge(
-                    self.nodes[up].out_edge(uport), dport)
-            self.nodes[up].on_submitted()
+        self.nodes[self._entrance].add_in_edge(
+            self._in_edge, self._entrance_port)
+        super().on_submitted()
+        self._out_edge = self.nodes[self._exit].out_edge(self._exit_port)
+        self._out_edge.fields.merge(self._in_edge.fields)
+        self._out_edge.fields.merge(self.fields)
+        self._out_edge.params.update(self._in_edge.params)
+        self._out_edge.params.update(self.params)
+
+    def add_in_edge(self, edge, port):
+        if port != 0:
+            raise ValueError("invalid port")
+        self._in_edge = edge
+
+    def out_edge(self, port):
+        if port != 0:
+            raise ValueError("invalid port")
+        return self._out_edge
+
+    def set_entrance(self, node, port=0):
+        if node.node_num is None:
+            node.node_num = len(self.nodes)
+            self.nodes.append(node)
+        self._entrance = node.node_num
+        self._entrance_port = port
+
+    def set_exit(self, node, port=0):
+        if node.node_num is None:
+            node.node_num = len(self.nodes)
+            self.nodes.append(node)
+        self._exit = node.node_num
+        self._exit_port = port
--- flashflood/test/core/test_workflow.py
+++ flashflood/test/core/test_workflow.py
@@ -11,26 +11,25 @@ from tornado.testing import AsyncTestCase, gen_test
 
 from flashflood.core.node import Asynchronizer
 from flashflood.core.container import Container
+from flashflood.node.function.apply import Apply
 from flashflood.node.reader.iterator import IteratorInput
 from flashflood.node.writer.container import (
     ContainerWriter, AsyncContainerWriter)
-from flashflood.core.workflow import Workflow
-
-
-def twice(dict_):
-    return {"value": dict_["value"] * 2}
+from flashflood.core.workflow import Workflow, SubWorkflow
 
 
 class TestWorkflow(AsyncTestCase):
     @gen_test
     def test_workflow(self):
         wf = Workflow()
-        wf.results = Container()
+        with self.assertRaises(ValueError):
+            yield wf.submit()
+        results = Container()
         iter_in = IteratorInput(range(10))
-        writer = ContainerWriter(wf.results)
+        writer = ContainerWriter(results)
         wf.connect(iter_in, writer)
         yield wf.submit()
-        self.assertEqual(sum(wf.results.records), 45)
+        self.assertEqual(sum(results.records), 45)
         self.assertTrue(all(n.status == "done" for n in wf.nodes))
 
     @gen_test
@@ -57,6 +56,25 @@ class TestWorkflow(AsyncTestCase):
         self.assertEqual(iter_in.status, "done")
         self.assertEqual(async.status, "aborted")
         self.assertEqual(writer.status, "aborted")
+
+    @gen_test
+    def test_subworkflow(self):
+        # sub
+        sub = SubWorkflow()
+        twice = Apply(lambda x: x * 2)
+        sub.set_entrance(twice)
+        sub.set_exit(twice)
+        # main
+        wf = Workflow()
+        results = Container()
+        iter_in = IteratorInput(range(10))
+        writer = ContainerWriter(results)
+        wf.connect(iter_in, sub)
+        wf.connect(sub, writer)
+        yield wf.submit()
+        self.assertEqual(sum(results.records), 90)
+        self.assertTrue(all(n.status == "done" for n in sub.nodes))
+        self.assertTrue(all(n.status == "done" for n in wf.nodes))
 
 
 if __name__ == '__main__':
