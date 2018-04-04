87e832fd4d6d22c6783cfc7c2e8d6ca6712b7ccd
Seiji Matsuoka <mojaie@aol.com>
2018-04-04 22:50:31 +0900

    httpfetch default parser

--- flashflood/node/reader/httpfetch.py
+++ flashflood/node/reader/httpfetch.py
@@ -12,7 +12,8 @@ from flashflood.node.reader.readerbase import ReaderBase
 
 
 class HttpFetchInput(ReaderBase):
-    def __init__(self, url, headers=None, response_parser=None, **kwargs):
+    def __init__(self, url, headers=None, response_parser=json.loads,
+                 **kwargs):
         super().__init__(**kwargs)
         self.url = url
         self.response_parser = response_parser
@@ -24,8 +25,6 @@ class HttpFetchInput(ReaderBase):
         request.headers = self.headers
         response = http_client.fetch(request)
         http_client.close()
-        res = json.loads(response.body.decode("utf-8"))
-        if self.response_parser is not None:
-            res = self.response_parser(res)
+        res = self.response_parser(response.body.decode("utf-8"))
         self._out_edge.send(res)
         on_finish()
