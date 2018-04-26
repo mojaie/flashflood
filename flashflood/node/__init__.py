615c0c4afdd3147646b97411b3056d1011c42926
Seiji Matsuoka <mojaie@aol.com>
2018-04-26 22:28:42 +0900

    node import shortcut

---
+++ flashflood/node/__init__.py
@@ -0,0 +1,52 @@
+from flashflood.node.aggregate.first import AggFirst
+from flashflood.node.aggregate.list import AggList
+from flashflood.node.aggregate.sum import AggSum
+from flashflood.node.aggregate.uniqlist import AggUniqList
+from flashflood.node.aggregate.update import AggUpdate
+
+from flashflood.node.chem.descriptor import MolDescriptor, AsyncMolDescriptor
+from flashflood.node.chem.molecule import (
+    MoleculeToJSON, AsyncMoleculeToJSON, MoleculeFromJSON,
+    PickleMolecule, UnpickleMolecule
+)
+from flashflood.node.control.filter import Filter, AsyncFilter
+
+from flashflood.node.field.concat import ConcatFields
+from flashflood.node.field.constant import ConstantField, AsyncConstantField
+from flashflood.node.field.extend import Extend, AsyncExtend
+from flashflood.node.field.extract import Extract
+from flashflood.node.field.number import Number, AsyncNumber
+from flashflood.node.field.remove import RemoveField, RemoveFields
+from flashflood.node.field.split import SplitField
+from flashflood.node.field.update import UpdateFields
+
+from flashflood.node.monitor.count import CountRows, AsyncCountRows
+from flashflood.node.monitor.stdout import StdoutMonitor, AsyncStdoutMonitor
+
+from flashflood.node.reader.csv import CSVFileReader
+from flashflood.node.reader.httpfetch import HttpFetchInput
+from flashflood.node.reader.iterinput import IterInput
+from flashflood.node.reader.sdfile import SDFileReader, SDFileLinesInput
+from flashflood.node.reader.sqlite import (
+    SQLiteReader, SQLiteReaderFilter, SQLiteReaderSearch
+)
+
+from flashflood.node.record.excludes import Excludes, AsyncExcludes
+from flashflood.node.record.filter import FilterRecords, AsyncFilterRecords
+from flashflood.node.record.includes import Includes, AsyncIncludes
+from flashflood.node.record.merge import MergeRecords, AsyncMergeRecords
+from flashflood.node.record.numericfilter import (
+    NumericFilter, AsyncNumericFilter
+)
+from flashflood.node.record.sort import NumericSort
+from flashflood.node.record.startswith import StartsWith, AsyncStartsWith
+
+from flashflood.node.transform.combination import Combination
+from flashflood.node.transform.join import LeftJoin
+from flashflood.node.transform.stack import Stack
+from flashflood.node.transform.unstack import Unstack
+
+from flashflood.node.writer.container import ContainerWriter
+from flashflood.node.writer.csv import CsvWriter
+from flashflood.node.writer.sdfile import SDFileWriter
+from flashflood.node.writer.sqlite import SQLiteWriter
