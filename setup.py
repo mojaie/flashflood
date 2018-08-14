92466994798416b542d682b1e1b5e0ecb8d7e05c
Seiji Matsuoka <mojaie@aol.com>
2018-08-14 09:13:17 +0900

    0.13.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="Flashflood",
-    version="0.13.0",
+    version="0.13.1",
     description="HTTP API server builder for chemical database workflow",
     long_description=long_description,
     url="https://github.com/mojaie/flashflood",
